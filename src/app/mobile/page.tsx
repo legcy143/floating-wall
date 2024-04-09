0aa112c18b5f34bac0ff5c42d1f5aeec283674f7
legcy <legcy143@gmail.com>
2024-04-09 09:54:46 +0530

    update input box

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -22,6 +22,7 @@ import { Textarea } from "@/components/ui/textarea";
 import React, { useEffect, useRef, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { toast } from "sonner";
+import { IoIosSend } from "react-icons/io";
 
 import io from "socket.io-client";
 import { TiArrowSortedDown } from "react-icons/ti";
@@ -100,9 +101,10 @@ export default function Mobile() {
             <CardTitle></CardTitle>
           </CardHeader>
           {/* content goes here */}
-          <section className="space-y-2 p-1 py-2 overflow-auto">
+          {/* <section className="space-y-2 p-1 py-2 overflow-auto">
             <div>
-              <Label>
+              <div className="flex items-center">
+              <Label className="w-32 bg-green--500">
                 Name <span className="text-red-500">*</span>
               </Label>
               <Input
@@ -110,18 +112,19 @@ export default function Mobile() {
                 value={formData.name}
                 onChange={(e) => handleOnChange("name", e.target.value)}
               />
+              </div>
               <span className="text-xs text-end w-full block m-1 ml-0">
                 {formData.name.length}/{maxLength.name}
               </span>
             </div>
 
-            <div className="flex gap-x-2 items-center">
-              <p>I'm Feeling </p>
+            <div className="flex gap-x-2 items-center bg-red-500">
+              <p className="w-32 bg-green-300">I'm Feeling </p>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <Button variant="outline">{formData.feedback}<TiArrowSortedDown className="ml-3"/></Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56 h-full">
+                <DropdownMenuContent className="w-full h-full">
                   <DropdownMenuLabel>I am Feeling</DropdownMenuLabel>
                   <DropdownMenuSeparator />
                   <DropdownMenuRadioGroup
@@ -141,10 +144,55 @@ export default function Mobile() {
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
+          </section> */}
+
+          {/* new update */}
+          <section className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <Label className="w-20">
+                Name <span className="text-red-500">*</span>
+              </Label>
+              <Input
+              className="flex-1"
+                placeholder=". . ."
+                value={formData.name}
+                onChange={(e) => handleOnChange("name", e.target.value)}
+                />
+                </div>
+              <span className="text-xs text-end w-full block ml-0 mt-[-0.5rem]">
+                {formData.name.length}/{maxLength.name}
+              </span>
+
+              <div className="flex items-center gap-2">
+              <Label className=" w-20">I'm Feeling </Label>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className=" flex-1 justify-between">{formData.feedback}<TiArrowSortedDown className="ml-3"/></Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-full h-full flex-1">
+                  <DropdownMenuLabel>I am Feeling</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuRadioGroup
+                    className="p-2 max-h-[40vh] overflow-y-auto md:scroll-m-10"
+                    defaultValue={formData.feedback}
+                    onValueChange={(e) => {
+                      handleOnChange("feedback", e);
+                      console.log(e);
+                    }}
+                  >
+                    {feedbackOptions?.map((e) => (
+                      <DropdownMenuRadioItem key={e} value={e}>
+                        {e}
+                      </DropdownMenuRadioItem>
+                    ))}
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+                    </div>
           </section>
           {/* content goes here */}
-          <CardFooter className="flex flex-row items-center justify-center gap-2 py-2">
-            <Button onClick={handleSubmit}>Share</Button>
+          <CardFooter className="flex flex-row items-center justify-center gap-2 py-2 pt-7">
+            <Button className="px-14 gap-2 text-white bg-[--event-theme]"  onClick={handleSubmit}>Share</Button>
           </CardFooter>
         </CardContent>
       </Card>
